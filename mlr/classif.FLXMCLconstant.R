5ce05de8d6bed8048c6fcdd8e287c268235aa483
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-03-04 20:17:22 +0000

    mlr interface code for mixtures of classifiers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ mlr/classif.FLXMCLconstant.R
@@ -0,0 +1,84 @@
+setClass(
+		"classif.FLXMCLconstant",
+		contains = c("rlearner.classif")
+)
+
+
+
+setMethod(
+		f = "initialize",
+		signature = signature("classif.FLXMCLconstant"),
+		def = function(.Object) {
+				par.set = makeParamSet(
+						## flexmix parameters
+						makeIntegerLearnerParam(id = "k", lower = 1, requires = expression(missing(cluster))),
+						makeIntegerVectorLearnerParam(id = "cluster", requires = expression(missing(k))),
+						## control
+						makeIntegerLearnerParam(id = "iter.max", lower = 1L, default = 200L),						
+						makeNumericLearnerParam(id = "minprior", lower = 0, upper = 1, default = 0.05),
+						makeNumericLearnerParam(id = "tolerance", lower = 0, default = 1e-06),	
+						makeIntegerLearnerParam(id = "verbose", lower = 0L, default = 0L),						
+						makeDiscreteLearnerParam(id = "classifiy", values = c("auto", "weighted", "hard", "CEM", "random", "SEM"), default = "auto"),						
+						makeIntegerLearnerParam(id = "nrep", lower = 1L, default = 1L)
+     		)		
+      		.Object = callNextMethod(.Object, pack = "locClass", par.set = par.set)
+			setProperties(.Object,
+					oneclass = FALSE,
+					twoclass = TRUE,
+					multiclass = TRUE,
+					missings = FALSE,
+					numerics = TRUE,
+					factors = TRUE,
+					prob = TRUE,
+					weights = TRUE
+			)			
+		}
+)
+
+
+
+setMethod(
+		f = "trainLearner",
+		signature = signature(
+				.learner = "classif.FLXMCLconstant",
+				.task = "ClassifTask",
+				.subset = "integer"
+		),
+		def = function(.learner, .task, .subset,  ...) {
+			f1 = getFormula(.task)
+			f2 = as.formula(paste("~ ", paste(getFeatureNames(.task), collapse = "+")))
+			model = FLXMCLconstant(...)
+			mf = match.call()
+    		mcontrol = match(c("iter.max", "minprior", "tolerance", "verbose", "classify", "nrep"), names(mf), 0)
+		    mfcontrol = mf[c(1, mcontrol)]
+    		mfcontrol[[1]] = as.name("list")
+			control = eval(mfcontrol)
+      		if (.task@desc@has.weights)
+        		flexmix(f1, data = getData(.task, .subset), weights = .task@weights[.subset], 
+        			concomitant = FLXPwlda(f2), model = model, control = control, 
+        			k = eval(mf$k), cluster = eval(mf$cluster))
+      		else
+        		flexmix(f1, data = getData(.task, .subset), 
+        			concomitant = FLXPwlda(f2), model = model, control = control,
+        			k = eval(mf$k), cluster = eval(mf$cluster))
+		}
+)   
+
+
+
+setMethod(
+		f = "predictLearner",
+		signature = signature(
+				.learner = "classif.FLXMCLconstant",
+				.model = "WrappedModel",
+				.newdata = "data.frame"
+		),
+		def = function(.learner, .model, .newdata, ...) {
+			lev = attr(.model@learner.model@model[[1]]@y, "lev")
+			p = predict(.model@learner.model, newdata = .newdata, local.aggregate = TRUE, ...)[[1]]
+			if (.learner@predict.type == "response") {
+				p = factor(colnames(p)[max.col(p)], levels = lev) ## does this always work?
+			}
+			return(p)			
+		}
+)
---
+++ mlr/classif.FLXMCLnnet.R
@@ -0,0 +1,104 @@
+setClass(
+		"classif.FLXMCLnnet",
+		contains = c("rlearner.classif")
+)
+
+
+
+setMethod(
+		f = "initialize",
+		signature = signature("classif.FLXMCLnnet"),
+		def = function(.Object) {
+				par.set = makeParamSet(
+						## flexmix parameters
+						makeIntegerLearnerParam(id = "k", lower = 1, requires = expression(missing(cluster))),
+						makeIntegerVectorLearnerParam(id = "cluster", requires = expression(missing(k))),
+						## control
+						makeIntegerLearnerParam(id = "iter.max", lower = 1L, default = 200L),						
+						makeNumericLearnerParam(id = "minprior", lower = 0, upper = 1, default = 0.05),
+						makeNumericLearnerParam(id = "tolerance", lower = 0, default = 1e-06),	
+						makeIntegerLearnerParam(id = "verbose", lower = 0L, default = 0L),						
+						makeDiscreteLearnerParam(id = "classifiy", values = c("auto", "weighted", "hard", "CEM", "random", "SEM"), default = "auto"),						
+						makeIntegerLearnerParam(id = "nrep", lower = 1L, default = 1L),
+					## todo: some may not be supported
+						## nnet parameters
+						# contrasts?
+						## nnet
+       	 				makeIntegerLearnerParam(id = "size", default = 3L, lower=0L, pass.default=TRUE),
+       	 				makeNumericVectorLearnerParam(id = "Wts"),
+       		 			#makeLogicalVectorLearnerParam(id = "mask"),#???
+        				# linout hard coded
+        				# entropy hard coded
+        				# softmax hard coded
+        				# censored hard coded       
+        				makeLogicalLearnerParam(id="skip", default=FALSE),
+        				makeNumericLearnerParam(id="rang", default=0.7),
+        				makeNumericLearnerParam(id="decay", default=0),
+        				makeIntegerLearnerParam(id="maxit", default=100L, lower=1L),
+        				makeLogicalLearnerParam(id="Hess", default=FALSE),
+        				makeLogicalLearnerParam(id="trace", default=TRUE),
+        				makeIntegerLearnerParam(id="MaxNWts", default=1000L),
+        				makeNumericLearnerParam(id="abstol", default=1.0e-4),
+        				makeNumericLearnerParam(id="reltol", default=1.0e-8)
+     		)
+      		.Object = callNextMethod(.Object, pack = "locClass", par.set = par.set)
+			setProperties(.Object,
+					oneclass = FALSE,
+					twoclass = TRUE,
+					multiclass = TRUE,
+					missings = FALSE,
+					numerics = TRUE,
+					factors = TRUE,
+					prob = TRUE,
+					weights = TRUE
+			)			
+		}
+)
+
+
+
+setMethod(
+		f = "trainLearner",
+		signature = signature(
+				.learner = "classif.FLXMCLnnet",
+				.task = "ClassifTask",
+				.subset = "integer"
+		),
+		def = function(.learner, .task, .subset,  ...) {
+			f1 = getFormula(.task)
+			f2 = as.formula(paste("~ ", paste(getFeatureNames(.task), collapse = "+")))
+			model = FLXMCLnnet(...)
+			mf = match.call()
+    		mcontrol = match(c("iter.max", "minprior", "tolerance", "verbose", "classify", "nrep"), names(mf), 0)
+		    mfcontrol = mf[c(1, mcontrol)]
+    		mfcontrol[[1]] = as.name("list")
+			control = eval(mfcontrol)
+      		if (.task@desc@has.weights)
+        		flexmix(f1, data = getData(.task, .subset), weights = .task@weights[.subset], 
+        			concomitant = FLXPwlda(f2), model = model, control = control, 
+        			k = eval(mf$k), cluster = eval(mf$cluster))
+      		else
+        		flexmix(f1, data = getData(.task, .subset), 
+        			concomitant = FLXPwlda(f2), model = model, control = control,
+        			k = eval(mf$k), cluster = eval(mf$cluster))
+		}
+)   
+
+
+
+setMethod(
+		f = "predictLearner",
+		signature = signature(
+				.learner = "classif.FLXMCLnnet",
+				.model = "WrappedModel",
+				.newdata = "data.frame"
+		),
+		def = function(.learner, .model, .newdata, ...) {
+			lev = attr(.model@learner.model@model[[1]]@y, "lev")
+			p = predict(.model@learner.model, newdata = .newdata, local.aggregate = TRUE, ...)[[1]]
+			if (.learner@predict.type == "response") {
+				p = factor(colnames(p)[max.col(p)], levels = lev) ## does this always work?
+			}
+			return(p)			
+		}
+)
