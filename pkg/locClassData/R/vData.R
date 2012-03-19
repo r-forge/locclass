9b734aaeafd9812fbb7dc124cad587e6ff43b8bc
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-03-19 13:04:28 +0000

    new version of locClassData package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/locClassData/R/vData.R
@@ -0,0 +1,138 @@
+#  Copyright (C) 2011 Julia Schiffner
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+#
+
+#' description
+#'
+#' details
+#'
+#' @title Create an Artificial Classification Problem with V-shaped decision boundary
+#'
+#' @param n Number of observations.
+#' @param d The dimensionality.
+#' @param k Parameter to adjust .
+#' @param data A \code{data.frame}.
+#'
+#' @return
+#' returns an object of class \code{"locClass"}, a list with components:
+#' \item{x}{(A matrix.) The explanatory variables.}
+#' \item{y}{(A factor.) The class labels.}
+#'
+#' @examples
+#' # Generate a training and a test set
+#' train <- vData(1000)
+#' test <- vData(1000)
+#'
+#' # Generate a grid of points
+#' x.1 <- x.2 <- seq(0.01,1,0.01)
+#' grid <- expand.grid(x.1 = x.1, x.2 = x.2)
+#'
+#' # Calculate the posterior probablities for all grid points
+#' gridPosterior <- vPosterior(grid)
+#'
+#' # Draw contour lines of posterior probabilities and plot training observations
+#' contour(x.1, x.2, matrix(gridPosterior[,1], length(x.1)), col = "gray")
+#' points(train$x, col = train$y)
+#'
+#' # Calculate Bayes error
+#' ybayes <- vBayesClass(test$x)
+#' mean(ybayes != test$y)
+#'
+#' if (require(MASS)) {
+#'	
+#' 	   # Fit an LDA model and calculate misclassification rate on the test data set
+#'     tr <- lda(y ~ ., data = as.data.frame(train))	
+#'     pred <- predict(tr, as.data.frame(test))	
+#'     mean(pred$class != test$y)
+#' 
+#'     # Draw decision boundary
+#'     gridPred <- predict(tr, grid)
+#'     contour(x.1, x.2, matrix(gridPred$posterior[,1], length(x.1)), col = "red", levels = 0.5, add = TRUE)
+#'
+#' }
+#'
+#' @aliases vData vLabels vPosterior vBayesClass
+#'
+#' @rdname vData
+#'
+#' @export
+#'
+
+vData <- function (n, d = 2, k = 1) {
+    data <- matrix(runif(d * n), nrow = n)
+	posterior <- 0.5 + k * (data[,2] - 2 * abs(data[,1] - 0.5))
+	posterior[posterior < 0] <- 0 
+	posterior[posterior > 1] <- 1 
+    y <- as.factor(sapply(posterior, function(x) sample(1:2, 
+        size = 1, prob = c(1 - x, x))))
+    data <- list(x = data, y = y)
+    class(data) <- c("locClass.vData", "locClass")
+    attr(data, "d") <- d
+    attr(data, "k") <- k
+    return(data)
+}
+
+
+
+#' @return returns a factor of class labels.
+#'
+#' @rdname vData
+#'
+#' @export
+
+vLabels <- function(data, k = 2) {
+	d <- ncol(data)
+	posterior <- 0.5 + k * (data[,2] - 2 * abs(data[,1] - 0.5))
+	posterior[posterior < 0] <- 0 
+	posterior[posterior > 1] <- 1 
+	classes <- as.factor(sapply(posterior, function(x) sample(1:2, size = 1, prob = c(1-x,x))))
+	return(classes)
+}	
+
+
+
+#' @return returns a matrix of posterior probabilities.
+#'
+#' @rdname vData
+#'
+#' @export
+
+vPosterior <- function(data, k = 2) {
+	d <- ncol(data)
+	posterior.2 <- 0.5 + k * (data[,2] - 2 * abs(data[,1] - 0.5))
+	posterior.2[posterior.2 < 0] <- 0 
+	posterior.2[posterior.2 > 1] <- 1 
+    posterior.1 <- 1 - posterior.2
+    posterior <- cbind(posterior.1, posterior.2)
+    colnames(posterior) <- paste("posterior", 1:d, sep = ".")
+	return(posterior)	
+}
+
+
+
+#' @return returns a factor of Bayes predictions.
+#'
+#' @rdname vData
+#'
+#' @export
+
+vBayesClass <- function(data, k = 2) {
+	d <- ncol(data)
+	posterior <- 0.5 + k * (data[,2] - 2 * abs(data[,1] - 0.5))
+	posterior[posterior < 0] <- 0 
+	posterior[posterior > 1] <- 1 
+	bayesclass <- as.factor(round(posterior) + 1)
+	return(bayesclass)
+}
---
+++ pkg/locClassData/R/vNormalLinearData.R
@@ -0,0 +1,123 @@
+#  Copyright (C) 2011 Julia Schiffner
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+#
+
+#' description
+#'
+#' details
+#'
+#' @title Create an Artificial Classification Problem with V-shaped decision boundary
+#'
+#' @param n Number of observations.
+#' @param prior Vector of class prior probabilities.
+#' @param lambda The conditional probabilities for the mixture components given the class. Either a vector (if the same number \eqn{m} 
+#'   of mixture components is desired for each class and the conditional probabilities for each class should be equal) or a list as long
+#'   as the number of classes containing one vector of probabilities for every class. The length of the \eqn{k}-th element is the desired 
+#'   number of mixture components for the \eqn{k}-th class.
+#' @param sigma The covariance matrices of the mixture components. Either one single matrix that is used for each mixture
+#'   component or a list as long as the number of classes. List elements can be matrices (in case that for all mixture components forming
+#'   one class the same covariance matrix shall be used) or lists of matrices as long as the number of mixture components in the corresponding 
+#'   class.
+#' @param data A \code{data.frame}.
+#'
+#' @return
+#' returns an object of class \code{"locClass"}, a list with components:
+#' \item{x}{(A matrix.) The explanatory variables.}
+#' \item{y}{(A factor.) The class labels.}
+#'
+#' @examples
+#' # Generate a training and a test set
+#' train <- vNormalData(1000)
+#' test <- vNormalData(1000)
+#'
+#' # Generate a grid of points
+#' x.1 <- x.2 <- seq(-5,5,0.1)
+#' grid <- expand.grid(x.1 = x.1, x.2 = x.2)
+#'
+#' # Calculate the posterior probablities for all grid points
+#' gridPosterior <- vNormalPosterior(grid)
+#'
+#' # Draw contour lines of posterior probabilities and plot training observations
+#' contour(x.1, x.2, matrix(gridPosterior[,1], length(x.1)), col = "gray")
+#' points(train$x, col = train$y)
+#'
+#' # Calculate Bayes error
+#' ybayes <- vNormalBayesClass(test$x)
+#' mean(ybayes != test$y)
+#'
+#' if (require(MASS)) {
+#'	
+#' 	   # Fit an LDA model and calculate misclassification rate on the test data set
+#'     tr <- lda(y ~ ., data = as.data.frame(train))	
+#'     pred <- predict(tr, as.data.frame(test))	
+#'     mean(pred$class != test$y)
+#' 
+#'     # Draw decision boundary
+#'     gridPred <- predict(tr, grid)
+#'     contour(x.1, x.2, matrix(gridPred$posterior[,1], length(x.1)), col = "red", levels = 0.5, add = TRUE)
+#'
+#' }
+#'
+#' @aliases vNormalData vNormalLabels vNormalPosterior vNormalBayesClass
+#'
+#' @rdname vNormalData
+#'
+#' @export
+#'
+
+vNormalData <- function(n, prior = rep(0.5,2), lambda = rep(0.5,2), sigma = diag(2)) {
+	mu <- list(rbind(c(-3,0), c(3,0)), rbind(c(-2,1), c(2,1)))
+	data <- mixtureData(n, prior, lambda, mu, sigma)
+	class(data) <- c("locClass.vNormalData", class(data))
+	return(data)
+}
+
+
+#' @return returns a factor of class labels.
+#'
+#' @rdname vNormalData
+#'
+#' @export
+
+vNormalLabels <- function(data, prior = rep(0.5,2), lambda = rep(0.5,2), sigma = diag(2)) {
+	mu <- list(rbind(c(-3,0), c(3,0)), rbind(c(-2,1), c(2,1)))
+	return(mixtureLabels(data, prior, lambda, mu, sigma))
+}	
+
+
+
+#' @return returns a matrix of posterior probabilities.
+#'
+#' @rdname vNormalData
+#'
+#' @export
+
+vNormalPosterior <- function(data, prior = rep(0.5,2), lambda = rep(0.5,2), sigma = diag(2)) {
+	mu <- list(rbind(c(-3,0), c(3,0)), rbind(c(-2,1), c(2,1)))
+	return(mixturePosterior(data, prior, lambda, mu, sigma))
+}
+
+
+
+#' @return returns a factor of Bayes predictions.
+#'
+#' @rdname vNormalData
+#'
+#' @export
+
+vNormalBayesClass <- function(data, prior = rep(0.5,2), lambda = rep(0.5,2), sigma = diag(2)) {
+	mu <- list(rbind(c(-3,0), c(3,0)), rbind(c(-2,1), c(2,1)))
+	return(mixtureBayesClass(data, prior, lambda, mu, sigma))
+}
