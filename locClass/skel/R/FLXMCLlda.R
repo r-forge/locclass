6796ce6601ca5089bd456f575ac1013e2ba3adff
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-10-11 15:47:35 +0000

    major bug fixes in mixtures of LDA and QDA
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@235 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClass/skel/R/FLXMCLlda.R
+++ locClass/skel/R/FLXMCLlda.R
@@ -52,10 +52,22 @@ setClass("FLXMCLlda", contains = "FLXMCL")
 #' 
 #' cluster <- kmeans(data$x, center = 2)$cluster
 #' model <- FLXMCLlda()
-#' fit <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = model, cluster = cluster)
+#' fit <- flexmix(y ~ ., data = as.data.frame(data), model = model, cluster = cluster, control = list(verb = 1))
 #' 
 #' ## prediction for single component models without aggregation
 #' pred.grid <- predict(fit, newdata = grid)
+#'
+#' # joint density of predictors and class variable for class 1
+#' image(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))))
+#' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))), add = TRUE)
+#' points(data$x, pch = as.character(data$y))
+#'
+#' image(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[2]][,1], length(seq(-6,6,0.2))))
+#' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[2]][,1], length(seq(-6,6,0.2))), add = TRUE)
+#' points(data$x, pch = as.character(data$y))
+#'
+#' # posterior probability of class 1
+#' pred.grid <- lapply(pred.grid, function(x) x/rowSums(x))
 #' image(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))))
 #' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))), add = TRUE)
 #' points(data$x, pch = as.character(data$y))
@@ -66,12 +78,21 @@ setClass("FLXMCLlda", contains = "FLXMCL")
 #'
 #' ## prediction with aggregation depending on membership in mixture components
 #' pred.grid <- mypredict(fit, newdata = grid, aggregate = TRUE)
+#'
+#' # joint density of predictors and class variable for class 1
+#' image(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))))
+#' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))), add  = TRUE)
+#' points(data$x, pch = as.character(data$y))
+#'
+#' # posterior of class 1
+#' pred.grid <- lapply(pred.grid, function(x) x/rowSums(x))
 #' image(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))))
 #' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(pred.grid[[1]][,1], length(seq(-6,6,0.2))), add  = TRUE)
 #' points(data$x, pch = as.character(data$y))
 #'
 #' ## local membership
-#' loc.grid <- prior(fit, newdata = grid)
+#' grid <- cbind(y = flashBayesClass(grid), grid)
+#' loc.grid <- posterior(fit, newdata = grid)
 #' contour(seq(-6,6,0.2), seq(-4,4,0.2), matrix(loc.grid[,1], length(seq(-6,6,0.2))), add  = TRUE)
 
 FLXMCLlda <- function(formula = . ~ ., method = c("unbiased", "ML"), ...) {
@@ -80,45 +101,35 @@ FLXMCLlda <- function(formula = . ~ ., method = c("unbiased", "ML"), ...) {
 		name = "Mixture of LDA models")
 	z@defineComponent <- expression({
 		predict <- function(x, ...) {
-			pred <- getS3method("predict", "wlda")(fit, newdata = x, ...)
-			lev <- levels(pred$class)
-			ng <- length(lev)
-			if (ng > ncol(pred$posterior)) {
-	        	posterior <- matrix(0, nrow(pred$posterior), ng)
-	        	rownames(posterior) <- rownames(pred$posterior)
-	        	colnames(posterior) <- lev
-	        	posterior[,colnames(pred$posterior)] <- pred$posterior
-			} else
-        		posterior <- pred$posterior
-        	return(posterior)
+			## returns unormalized posteriors !!!
+    		ng <- length(attr(y, "lev"))
+			lev1 <- names(fit$prior)
+    		post <- matrix(0, ncol = ng, nrow = nrow(x), dimnames = list(rownames(x), attr(y, "lev")))
+    		post[,lev1] <- sapply(lev1, function(z) fit$prior[z] * dmvnorm(x, fit$means[z,], fit$cov))
+	    	return(post)
 		}
 		logLik <- function(x, y, ...) {
-    		post <- getS3method("predict", "wlda")(fit, newdata = x, ...)$posterior
+			## unnormalized log posterior, joint log likelihood
     		ng <- length(attr(y, "lev"))
-# print(head(post))
-# print(head(y))
-    		if (ng > ncol(post)) {
-    			ll <- rep(0, nrow(post))
-    			col.index <- match(y, colnames(post), 0)
-    			row.index <- which(col.index > 0)
-    			ll[row.index] <- post[cbind(row.index, col.index[row.index])]
-    		} else {
-	    		ll <- post[cbind(rownames(post), as.character(y))]
-	    	}
-# print(head(ll))
+			lev1 <- names(fit$prior)
+    		post <- matrix(-10000, ncol = ng, nrow = nrow(x), dimnames = list(rownames(x), attr(y, "lev")))
+    		post[,lev1] <- sapply(lev1, function(z) log(fit$prior[z]) + dmvnorm(x, fit$means[z,], fit$cov, log = TRUE))
+#print(head(post))
+    		ll <- post[cbind(rownames(post), as.character(y))]
+#print(head(ll))
 	    	return(ll)
 		}
 		new("FLXcomponent", parameters = list(prior = fit$prior, means = fit$means, cov = fit$cov), 
 			logLik = logLik, predict = predict, df = fit$df)
 	})
 	z@preproc.y <- function(grouping) {
-    	if (!is.factor(grouping)) 
+    	if (!is.factor(grouping))
         	warning("'grouping' was coerced to a factor")
 	    g <- as.factor(grouping)
-	    lev <- levels(grouping)
+	    lev <- levels(g)
     	g <- as.matrix(g)
     	attr(g, "lev") <- lev
-		g
+		return(g)
 	}
 	z@fit <- function(x, y, w) {
 		fit <- wlda(x, factor(y, levels = attr(y, "lev")), weights = w, method = method)
@@ -129,6 +140,7 @@ FLXMCLlda <- function(formula = . ~ ., method = c("unbiased", "ML"), ...) {
 	}
 	z
 }
+
 
 
 #' @rdname FLXMCLlda
@@ -172,3 +184,74 @@ setMethod("FLXgetModelmatrix", signature(model = "FLXMCLlda"),
     model@xlevels <- .getXlevels(model@terms, mf)
     model
 })
+
+
+## old version
+# FLXMCLlda <- function(formula = . ~ ., method = c("unbiased", "ML"), ...) {
+	# method <- match.arg(method)
+	# z <- new("FLXMCLlda", weighted = TRUE, formula = formula,
+		# name = "Mixture of LDA models")
+	# z@defineComponent <- expression({
+		# predict <- function(x, ...) {
+			# pred <- getS3method("predict", "wlda")(fit, newdata = x, ...)
+			# lev <- levels(pred$class)
+			# ng <- length(lev)
+			# if (ng > ncol(pred$posterior)) {
+	        	# posterior <- matrix(0, nrow(pred$posterior), ng)
+	        	# rownames(posterior) <- rownames(pred$posterior)
+	        	# colnames(posterior) <- lev
+	        	# posterior[,colnames(pred$posterior)] <- pred$posterior
+			# } else
+        		# posterior <- pred$posterior
+        	# return(posterior)
+		# }
+		# logLik <- function(x, y, ...) {
+			# ## log posterior
+    		# post <- getS3method("predict", "wlda")(fit, newdata = x, ...)$posterior
+    		# ng <- length(attr(y, "lev"))
+# # print(head(post))
+# # print(head(y))
+    		# if (ng > ncol(post)) {
+    			# ll <- rep(0, nrow(post))
+    			# col.index <- match(y, colnames(post), 0)
+    			# row.index <- which(col.index > 0)
+    			# ll[row.index] <- post[cbind(row.index, col.index[row.index])]
+    		# } else {
+	    		# ll <- post[cbind(rownames(post), as.character(y))]
+	    	# }
+	    	# ll <- ifelse(ll == 0, -10000, log(ll))
+# # print(head(ll))
+	    	# return(ll)
+
+			# # #unnormalized log posterior, joint log likelihood
+    		# # ng <- length(attr(y, "lev"))
+			# # lev1 <- names(fit$prior)
+    		# # post <- matrix(-10000, ncol = ng, nrow = nrow(x), dimnames = list(rownames(x), attr(y, "lev")))
+    		# # post[,lev1] <- sapply(lev1, function(z) log(fit$prior[z]) + dmvnorm(x, fit$means[z,], fit$cov, log = TRUE))
+    		# # #post[,lev1] <- sapply(lev1, function(z) log(fit$prior[z]) - 0.5 * mahalanobis(x, center = fit$means[z,], cov = fit$cov))
+# # #print(head(post))
+    		# # ll <- post[cbind(rownames(post), as.character(y))]
+# # #print(head(ll))
+	    	# # return(ll)
+		# }
+		# new("FLXcomponent", parameters = list(prior = fit$prior, means = fit$means, cov = fit$cov), 
+			# logLik = logLik, predict = predict, df = fit$df)
+	# })
+	# z@preproc.y <- function(grouping) {
+    	# if (!is.factor(grouping))
+        	# warning("'grouping' was coerced to a factor")
+	    # g <- as.factor(grouping)
+	    # lev <- levels(g)
+    	# g <- as.matrix(g)
+    	# attr(g, "lev") <- lev
+		# return(g)
+	# }
+	# z@fit <- function(x, y, w) {
+		# fit <- wlda(x, factor(y, levels = attr(y, "lev")), weights = w, method = method)
+		# K <- nrow(fit$means)
+		# d <- ncol(fit$means)
+		# fit$df <- K*d + d*(d-1)/2
+		# with(fit, eval(z@defineComponent))
+	# }
+	# z
+# }
