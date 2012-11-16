a7ae256b7e9640d6d3325a3a9dbde213025d5706
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-11-16 20:40:22 +0000

    new initialization scheme for EM algorithm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@243 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClassMlr/skel/inst/tests/test_FLXMCLqda.R
+++ locClassMlr/skel/inst/tests/test_FLXMCLqda.R
@@ -7,9 +7,6 @@ test_that("FLXMCLqda: mlr interface works", {
 	data <- xor3Data(500)
 	task <- makeClassifTask(data = as.data.frame(data), target = "y")
 
-	set.seed(120)
-	cluster <- kmeans(data$x, centers = 3)$cluster
-
 	# class prediction
 	set.seed(120)
 	lrn <- makeLearner("classif.FLXMCLqda", centers = 3, method = "ML")
@@ -27,10 +24,13 @@ test_that("FLXMCLqda: mlr interface works", {
 	mean(pred1$data$response != pred2$data$truth)
 	mean(pred2$data$response != pred2$data$truth)
 
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLqda(method = "ML"), cluster = cluster, control = list(iter.max = 200))
+	set.seed(120)
+	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(method = "ML"), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(method = "ML"), cluster = posterior(tr3), control = list(iter.max = 200))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
-	expect_true(all(pred3[[1]] == pred2$data[,3:4]))
+	expect_true(all(pred3[[1]]/rowSums(pred3[[1]]) == pred2$data[,3:5]))
 	
 	# class prediction
 	set.seed(120)
@@ -49,9 +49,12 @@ test_that("FLXMCLqda: mlr interface works", {
 	mean(pred1$data$response != pred2$data$truth)
 	mean(pred2$data$response != pred2$data$truth)
 
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLqda(), cluster = cluster, control = list(iter.max = 200, classify = "hard"))
+	set.seed(120)
+	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2, classify = "hard"))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(), cluster = posterior(tr3), control = list(iter.max = 200, classify = "hard"))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
-	expect_true(all(pred3[[1]] == pred2$data[,3:5]))
+	expect_true(all(pred3[[1]]/(rowSums(pred3[[1]])) == pred2$data[,3:5]))
 	
 })
