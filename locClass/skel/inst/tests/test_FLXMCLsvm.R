8abae2b7b923d1d71e63e83a1ce31eecdbf6d355
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-11-23 21:57:04 +0000

    additional tests to check whether optional parameters are passed correctly to wsvm; removed FLXPwlda
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@265 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClass/skel/inst/tests/test_FLXMCLsvm.R
+++ locClass/skel/inst/tests/test_FLXMCLsvm.R
@@ -3,49 +3,75 @@ context("FLXMCLsvm")
 test_that("FLXMCLsvm: misspecified arguments", {
 	# wrong variable names
 	cluster <- kmeans(iris[,1:4], centers = 3)$cluster
-	expect_that(fit <- flexmix(Species ~ V1, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'V1' nicht gefunden"))
-	expect_that(fit <- flexmix(Species ~ Sepal.Length, data = iris, concomitant = FLXPwlda(~ V1), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'V1' nicht gefunden"))
-	expect_that(fit <- flexmix(y ~ Sepal.Length, data = iris, concomitant = FLXPwlda(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'y' nicht gefunden"))
+	expect_that(fit <- flexmix(Species ~ V1, data = iris, concomitant = FLXPmultinom(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'V1' nicht gefunden"))
+	expect_that(fit <- flexmix(Species ~ Sepal.Length, data = iris, concomitant = FLXPmultinom(~ V1), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'V1' nicht gefunden"))
+	expect_that(fit <- flexmix(y ~ Sepal.Length, data = iris, concomitant = FLXPmultinom(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("Objekt 'y' nicht gefunden"))
 	# wrong class
-	expect_error(fit <- flexmix(iris, data = iris, concomitant = FLXPwlda(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")))
+	expect_error(fit <- flexmix(iris, data = iris, concomitant = FLXPmultinom(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")))
 	# target variable also in x
 	# expect_error(mob(Species ~ Species + Sepal.Length | Sepal.Width, data = iris, model = constantModel,
 	#	control = mob_control(objfun = deviance, minsplit = 20)))	## funktioniert, sollte aber nicht
 })
 
 
-test_that("FLXMCLlda with several options works",{
-	cluster <- kmeans(iris[,1:4], centers = 2)$cluster
+test_that("FLXMCLsvm: arguments are passed to wsvm",{
+	iris[,1:4] <- scale(iris[,1:4])
+	set.seed(123)
+	cluster <- kmeans(iris[,"Sepal.Width"], centers = 3)$cluster
 
-	## weighted, FLXPwlda
-	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted", verb = 1))
-	# ok
+	## kernel, cost
+	fit <- flexmix(Species ~ Sepal.Width, data = iris, model = FLXMCLsvm(kernel = "linear", cost = 2), cluster = cluster, control = list(iter.max = 50, classify = "weighted", verb = 1))
+	expect_equal(fit@components[[1]][[1]]@parameters$kernel, 0)
+	expect_equal(fit@components[[1]][[1]]@parameters$cost, 2)
+	expect_true(fit@components[[1]][[1]]@parameters$fitted)
+
+	## degree, coef0
+	fit <- flexmix(Species ~ Sepal.Width, data = iris, model = FLXMCLsvm(kernel = "polynomial", coef0 = 2, degree = 2, fitted = TRUE, tolerance = 10^-5), cluster = cluster, control = list(iter.max = 50, classify = "weighted", verb = 1))
+	expect_equal(fit@components[[1]][[1]]@parameters$kernel, 1)
+	expect_equal(fit@components[[1]][[1]]@parameters$coef0, 2)
+	expect_equal(fit@components[[1]][[1]]@parameters$degree, 2)
+	expect_true(fit@components[[1]][[1]]@parameters$fitted)
+
+	## gamma
+	fit <- flexmix(Species ~ Sepal.Width, data = iris, model = FLXMCLsvm(kernel = "radial", gamma = 2, fitted = FALSE, tolerance = 10^-5), cluster = cluster, control = list(iter.max = 50, classify = "weighted", verb = 1))
+	expect_equal(fit@components[[1]][[1]]@parameters$kernel, 2)
+	expect_equal(fit@components[[1]][[1]]@parameters$gamma, 2)
+	expect_true(!fit@components[[1]][[1]]@parameters$fitted)
+})
+
+
+test_that("FLXMCLsvm with several options works",{
+	cluster <- kmeans(iris[,1], centers = 2)$cluster
+
+	# ## weighted, FLXPwlda
+	# fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted", verb = 1))
+	# # ok
 		
-	## hard, FLXPwlda
-	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard", verb = 1))
-	# not monotone, empty classes
+	# ## hard, FLXPwlda
+	# fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard", verb = 1))
+	# # not monotone, empty classes
 
 	## weighted, FLXPmultinom
 	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPmultinom(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted", verb = 1))
 	# ok
 	
-	## hard, FLXPwldamultinom
+	## hard, FLXPmultinom
 	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPmultinom(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard", verb = 1))
-	# not monotone, empty groups
+	# ok
 })
 
 
 test_that("FLXMCLsvm throws a warning if grouping variable is numeric", {
 	iris$Species <- as.numeric(iris$Species)
 	cluster <- kmeans(iris[,c(1,3)], centers = 2)$cluster
-	expect_that(tr2 <- flexmix(Species ~ Sepal.Length, data = iris, concomitant = FLXPwlda(~ Petal.Length + Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted")), gives_warning("'grouping' was coerced to a factor"))
+	expect_that(tr2 <- flexmix(Species ~ Sepal.Length, data = iris, concomitant = FLXPmultinom(~ Petal.Length + Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE, tolerance = 10^-6), cluster = cluster, control = list(iter.max = 50, classify = "weighted", tolerance = 10^-4, verb = 1)), gives_warning("'grouping' was coerced to a factor"))
 	## use weighted here instead of hard, otherwise SVM does not work
 })
 
 
 test_that("FLXMCLsvm works if only one predictor variable is given", {
 	cluster <- kmeans(iris[,1:4], centers = 2)$cluster
-	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted"))
+	fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPmultinom(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "weighted", verb = 1))
 })
 
 
@@ -61,14 +87,14 @@ test_that("FLXMCLsvm: Local and global solution coincide if only one cluster is 
 
 test_that("FLXMCLsvm: training data from only one class", {
 	cluster <- kmeans(iris[1:50,1:4], centers = 3)$cluster
-	expect_that(fit <- flexmix(Species ~ Sepal.Width + Sepal.Length, data = iris[1:50,], concomitant = FLXPwlda(~ Sepal.Width + Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("need training data from at least two classes"))
+	expect_that(fit <- flexmix(Species ~ Sepal.Width + Sepal.Length, data = iris[1:50,], concomitant = FLXPmultinom(~ Sepal.Width + Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), throws_error("need training data from at least two classes"))
 })
 
 
 test_that("FLXMCLsvm: missing classes in individual clusters", {
 	set.seed(123)
-	cluster <- kmeans(iris[,1:4], centers = 2)$cluster
-	expect_that(fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPwlda(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard")), gives_warning("some groups are empty"))
+	cluster <- kmeans(iris[,1], centers = 2)$cluster
+	expect_that(fit <- flexmix(Species ~ Sepal.Width, data = iris, concomitant = FLXPmultinom(~ Sepal.Length), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 100, classify = "hard")), gives_warning("some groups are empty"))
 	expect_equal(ncol(fit@components$Comp.1[[1]]@parameters$coefs), 2)
 	expect_equal(ncol(fit@components$Comp.2[[1]]@parameters$coefs), 1)
 	pred <- mypredict(fit, aggregate = FALSE)
@@ -119,9 +145,9 @@ test_that("FLXMCLsvm: removing clusters works", {
 	library(locClassData)
 	data <- flashData(500)
 	cluster <- kmeans(data$x, centers = 12)$cluster
-	tr2 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLsvm(), cluster = cluster, control = list(iter.max = 100))
-	expect_equal(length(tr2@components), 9)
-	expect_equal(ncol(tr2@posterior$scaled), 9)
+	tr2 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLsvm(tolerance = 10^-6), cluster = cluster, control = list(iter.max = 100, verb = 1))
+	expect_equal(length(tr2@components), 8)
+	expect_equal(ncol(tr2@posterior$scaled), 8)
 })
 
 
@@ -134,7 +160,7 @@ test_that("predict FLXMCLsvm works correctly with missing newdata", {
 	library(locClassData)
 	data <- flashData(500)
 	cluster <- kmeans(data$x[ran,], centers = 2)$cluster
-	tr2 <- flexmix(y ~ ., data = as.data.frame(data)[ran,], concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
+	tr2 <- flexmix(y ~ ., data = as.data.frame(data)[ran,], concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
 	pred1 <- mypredict(tr2, aggregate = FALSE)
 	pred2 <- mypredict(tr2, aggregate = FALSE, newdata = as.data.frame(data)[ran,])
 	expect_equal(pred1, pred2)
@@ -150,7 +176,7 @@ test_that("predict FLXMCLsvm works correctly with missing newdata", {
 test_that("predict FLXMCLsvm works with missing classes in the training data", {
 	ran <- sample(1:150,100)
 	cluster <- kmeans(iris[1:100,1:4], centers = 2)$cluster
-	expect_that(tr2 <- flexmix(Species ~ ., data = iris[1:100,], concomitant = FLXPwlda(~ Sepal.Width + Sepal.Length), model = FLXMCLsvm(), cluster = cluster, control = list(iter.max = 200)), gives_warning("some groups are empty"))
+	expect_that(tr2 <- flexmix(Species ~ ., data = iris[1:100,], concomitant = FLXPmultinom(~ Sepal.Width + Sepal.Length), model = FLXMCLsvm(), cluster = cluster, control = list(iter.max = 200)), gives_warning("some groups are empty"))
 	pred <- mypredict(tr2, aggregate = FALSE)
 	expect_equal(ncol(pred[[1]]), 3) #!!!
 	expect_equal(ncol(pred[[2]]), 3) #!!!
@@ -165,7 +191,7 @@ test_that("predict FLXMCLsvm works with missing classes in the training data", {
 test_that("predict FLXMCLsvm works with one single predictor variable", {
 	ran <- sample(1:150,100)
 	cluster <- kmeans(iris[ran,1:4], centers = 2)$cluster
-	tr2 <- flexmix(Species ~ Sepal.Width, data = iris[ran,], concomitant = FLXPwlda(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
+	tr2 <- flexmix(Species ~ Sepal.Width, data = iris[ran,], concomitant = FLXPmultinom(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
 	pred <- mypredict(tr2, newdata = iris[-ran,], aggregate = FALSE)
 	pred <- mypredict(tr2, aggregate = TRUE)
 })
@@ -175,7 +201,7 @@ test_that("predict FLXMCLsvm works with one single test observation", {
 	set.seed(120)
 	ran <- sample(1:150,100)
 	cluster <- kmeans(iris[ran,1:4], centers = 2)$cluster
-	tr2 <- flexmix(Species ~ Sepal.Width, data = iris[ran,], concomitant = FLXPwlda(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
+	tr2 <- flexmix(Species ~ Sepal.Width, data = iris[ran,], concomitant = FLXPmultinom(~ Sepal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
   	pred <- mypredict(tr2, newdata = iris[5,])
 	expect_equal(dim(pred[[1]]), c(1,3))
 	expect_equal(dim(pred[[2]]), c(1,3))
@@ -187,7 +213,7 @@ test_that("predict FLXMCLsvm works with one single test observation", {
 test_that("predict FLXMCLsvm: NA handling in newdata works", {
 	ran <- sample(1:150,100)
 	cluster <- kmeans(iris[ran,1:4], centers = 2)$cluster
-	tr2 <- flexmix(Species ~ Sepal.Length + Sepal.Width, data = iris[ran,], concomitant = FLXPwlda(~ Sepal.Width + Petal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
+	tr2 <- flexmix(Species ~ Sepal.Length + Sepal.Width, data = iris[ran,], concomitant = FLXPmultinom(~ Sepal.Width + Petal.Width), model = FLXMCLsvm(kernel = "linear", fitted = FALSE), cluster = cluster, control = list(iter.max = 200))
 
 	## NAs in explanatory variables are ok
 	irisna <- iris
@@ -203,7 +229,7 @@ test_that("predict FLXMCLsvm: NA handling in newdata works", {
 	pred <- mypredict(tr2, newdata = irisna)
 	expect_equal(all(is.na(pred[[1]][1:17,])), TRUE)
 	expect_equal(all(is.na(pred[[2]][1:17,])), TRUE)
-	expect_warning(pred <- mypredict(tr2, newdata = irisna, aggregate = TRUE))
+	pred <- mypredict(tr2, newdata = irisna, aggregate = TRUE)
 	expect_equal(all(is.na(pred[[1]][1:17,])), TRUE)
 })
 
@@ -211,7 +237,7 @@ test_that("predict FLXMCLsvm: NA handling in newdata works", {
 test_that("predict FLXMCLsvm: misspecified arguments", {
 	ran <- sample(1:150,100)
 	cluster <- kmeans(iris[ran,1:4], centers = 2)$cluster
-	tr2 <- flexmix(Species ~ Sepal.Width + Petal.Width, data = iris[ran,], concomitant = FLXPwlda(~ Sepal.Width + Petal.Width), model = FLXMCLsvm(), cluster = cluster, control = list(iter.max = 200))
+	tr2 <- flexmix(Species ~ Sepal.Width + Petal.Width, data = iris[ran,], concomitant = FLXPmultinom(~ Sepal.Width + Petal.Width), model = FLXMCLsvm(), cluster = cluster, control = list(iter.max = 200))
     # errors in newdata
     expect_error(mypredict(tr2, newdata = TRUE))
     expect_error(mypredict(tr2, newdata = -50:50))
