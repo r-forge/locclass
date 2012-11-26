864eca8a17717454f685225d17bf6e34fc889ec8
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-11-26 20:43:18 +0000

    added tests to make sure that optional parameters are passed correctly; removed 'method = ML' since hard-coded in FLXMCLqda
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClassMlr/skel/inst/tests/test_FLXMCLqda.R
+++ locClassMlr/skel/inst/tests/test_FLXMCLqda.R
@@ -1,21 +1,30 @@
 context("FLXMCLqda: mlr interface code")
 
 test_that("FLXMCLqda: mlr interface works", {
-	library(mlr)
+	library(locClassData)
 
-	set.seed(150)
 	data <- xor3Data(500)
 	task <- makeClassifTask(data = as.data.frame(data), target = "y")
 
+	#### model parameters are passed
+	# centers, method
+	lrn <- makeLearner("classif.FLXMCLqda", centers = 3)
+	tr1 <- train(lrn, task)
+	expect_equal(length(tr1$learner.model@components), 3)
+	expect_equal(tr1$learner.model@components[[1]][[1]]@parameters$method, "ML")
+
+	expect_error(lrn <- makeLearner("classif.FLXMCLqda", centers = 3, method = "unbiased"))
+
+	## weighted
 	# class prediction
 	set.seed(120)
-	lrn <- makeLearner("classif.FLXMCLqda", centers = 3, method = "ML")
+	lrn <- makeLearner("classif.FLXMCLqda", centers = 3)
 	tr1 <- train(lrn, task)
 	pred1 <- predict(tr1, task = task)
 	
 	# posterior prediction
 	set.seed(120)
-	lrn <- makeLearner("classif.FLXMCLqda", par.vals = list(method = "ML", centers = 3, iter.max = 200), predict.type = "prob")
+	lrn <- makeLearner("classif.FLXMCLqda", par.vals = list(centers = 3, iter.max = 200), predict.type = "prob")
 	tr2 <- train(lrn, task)
 	pred2 <- predict(tr2, task = task)
 
@@ -26,12 +35,13 @@ test_that("FLXMCLqda: mlr interface works", {
 
 	set.seed(120)
 	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
-	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(method = "ML"), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(method = "ML"), cluster = posterior(tr3), control = list(iter.max = 200))
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), model = FLXMCLqda(), cluster = posterior(tr3), control = list(iter.max = 200))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
 	expect_true(all(pred3[[1]]/rowSums(pred3[[1]]) == pred2$data[,3:5]))
 	
+	# hard
 	# class prediction
 	set.seed(120)
 	lrn <- makeLearner("classif.FLXMCLqda", centers = 3, classify = "hard")
