4b90f93959c746926e1be50abce395e581befb12
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2012-11-26 21:05:11 +0000

    added decay parameter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@271 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClassMlr/skel/inst/tests/test_FLXMCLmultinom.R
+++ locClassMlr/skel/inst/tests/test_FLXMCLmultinom.R
@@ -26,13 +26,13 @@ test_that("FLXMCLmultinom: mlr interface works", {
 	## weighted
 	# class prediction
 	set.seed(120)
-	lrn <- makeLearner("classif.FLXMCLmultinom", centers = 3, trace = TRUE)
+	lrn <- makeLearner("classif.FLXMCLmultinom", centers = 3, decay = 0.1, trace = TRUE)
 	tr1 <- train(lrn, task)
 	pred1 <- predict(tr1, task = task)
 	
 	# posterior prediction
 	set.seed(120)
-	lrn <- makeLearner("classif.FLXMCLmultinom", par.vals = list(trace = FALSE, centers = 3, iter.max = 200), predict.type = "prob")
+	lrn <- makeLearner("classif.FLXMCLmultinom", par.vals = list(trace = FALSE, centers = 3, decay = 0.1, iter.max = 200), predict.type = "prob")
 	tr2 <- train(lrn, task)
 	pred2 <- predict(tr2, task = task)
 
@@ -43,8 +43,8 @@ test_that("FLXMCLmultinom: mlr interface works", {
 
 	set.seed(120)
 	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
-	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLmultinom(trace = FALSE), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLmultinom(trace = FALSE), cluster = posterior(tr3), control = list(iter.max = 200))
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLmultinom(trace = FALSE, decay = 0.1), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLmultinom(trace = FALSE, decay = 0.1), cluster = posterior(tr3), control = list(iter.max = 200))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
 	expect_true(all(pred3[[1]] == pred2$data[,3:5]))
