5f52a817e2da55187ae287e5a8c1eaabee4856bd
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2013-01-11 16:22:20 +0000

    added tests for argument 'reps'; adapted tests to improved initialization strategy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClassMlr/skel/inst/tests/test_FLXMCLnnet.R
+++ locClassMlr/skel/inst/tests/test_FLXMCLnnet.R
@@ -8,9 +8,28 @@ test_that("FLXMCLnnet: mlr interface works", {
 
 	Wts <- runif(9,-0.5, 0.5)
 
-	set.seed(120)
-	cluster <- kmeans(data$x, centers = 3)$cluster
+	#### model parameters are passed
+	# size, decay, trace, censored
+	# default: reps = 1
+	lrn <- makeLearner("classif.FLXMCLnnet", centers = 3, trace = FALSE, size = 2, decay = 0.2)
+	tr1 <- train(lrn, task)
+	expect_equal(length(tr1$learner.model@components), 3)
+	pars <- tr1$learner.model@components[[1]][[1]]@parameters
+	expect_equal(pars$decay, 0.2)
+	expect_equal(pars$n[2], 2)
+	expect_true(!pars$censored)
+	expect_equal(pars$reps, 1)
+	# reps = 2	
+	lrn <- makeLearner("classif.FLXMCLnnet", centers = 3, trace = FALSE, size = 1, reps = 2)
+	tr1 <- train(lrn, task)
+	expect_equal(length(tr1$learner.model@components), 3)
+	pars <- tr1$learner.model@components[[1]][[1]]@parameters
+	expect_equal(pars$decay, 0)
+	expect_equal(pars$n[2], 1)
+	expect_true(!pars$censored)
+	expect_equal(pars$reps, 2)
 
+	## weighted	
 	# class prediction
 	set.seed(120)
 	lrn <- makeLearner("classif.FLXMCLnnet", centers = 3, Wts = Wts, size = 1, trace = FALSE)
@@ -28,11 +47,15 @@ test_that("FLXMCLnnet: mlr interface works", {
 	mean(pred2$data$response != pred1$data$truth)
 	mean(pred2$data$response != pred2$data$truth)
 	
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = cluster, control = list(iter.max = 200))
+	set.seed(120)
+	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = cluster, control = list(iter.max = 200, tolerance = 10^-2))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = posterior(tr3), control = list(iter.max = 200))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
 	expect_true(all(pred3[[1]] == pred2$data[,3:5]))
 	
+	## hard
 	# class prediction
 	set.seed(120)
 	lrn <- makeLearner("classif.FLXMCLnnet", centers = 3, Wts = Wts, size = 1, trace = FALSE, classify = "hard")
@@ -47,7 +70,10 @@ test_that("FLXMCLnnet: mlr interface works", {
 
 	expect_equal(pred1$data$response, pred2$data$response)
 
-	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPwlda(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard"))
+	set.seed(120)
+	cluster <- replicate(5, kmeans(data$x, centers = 3)$cluster)
+	tr3 <- myStepFlexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = cluster, control = list(iter.max = 200, classify = "hard", tolerance = 10^-2))
+	tr3 <- flexmix(y ~ ., data = as.data.frame(data), concomitant = FLXPmultinom(~ x.1 + x.2), model = FLXMCLnnet(Wts = Wts, size = 1, trace = FALSE), cluster = posterior(tr3), control = list(iter.max = 200, classify = "hard"))
 	pred3 <- mypredict(tr3, aggregate = TRUE)
 
 	expect_true(all(pred3[[1]] == pred2$data[,3:5]))
