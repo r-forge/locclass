773070cc3d663489d4264cb1057edb4c342c585d
schiffner (Julia Schiffner) <schiffner@statistik.tu-dortmund.de>
2013-02-22 09:49:57 +0000

    removed print commands
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/locclass@355 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- locClassData/skel/R/flexibleData.R
+++ locClassData/skel/R/flexibleData.R
@@ -169,21 +169,6 @@ flexibleDataParams <- function(n, probMix, centersMix, sigmaMix = 0.2, centersOt
 		nc <- floor(centersOther * 0.5)
 		muOther[1:nc,] <- matrix(runif(nc*d, -0.25, 0.25), nc, d)
 		muOther[(nc+1):centersOther,] <- matrix(runif((centersOther-nc)*d, -1, 1), centersOther-nc, d)		
-		bayesClassOther <- mixtureLabels(muOther, prior = prior, mu = muMix, sigma = lapply(sigmaMix, function(x) x*diag(d)), lambda = lambdaMix)
-print(bayesClassOther)	
-		z <- 0
-		while (length(unique(as.numeric(bayesClassOther))) < K) {
-			muOther <- matrix(0, centersOther, d)
-			nc <- floor(centersOther * 0.5)
-			muOther[1:nc,] <- matrix(runif(nc*d, -0.25, 0.25), nc, d)
-			muOther[(nc+1):centersOther,] <- matrix(runif((centersOther-nc)*d, -1, 1), centersOther-nc, d)		
-			bayesClassOther <- mixtureLabels(muOther, prior = prior, mu = muMix, sigma = lapply(sigmaMix, function(x) x*diag(d)), lambda = lambdaMix)
-print(bayesClassOther)				
-			z <- z + 1
-print(z)
-			if (z > 25)
-				break
-		}
 		return(list(prior = prior, K = K, d = d, dUseless = dUseless, probMix = probMix, nMix = nMix, muMix = muMix, sigmaMix = sigmaMix, lambdaMix = lambdaMix, nOther = nOther, muOther = muOther, sigmaOther = sigmaOther))
 	} else {
 		return(list(prior = prior, K = K, d = d, dUseless = dUseless, probMix = probMix, nMix = nMix, muMix = muMix, sigmaMix = sigmaMix, lambdaMix = lambdaMix, nOther = nOther))
@@ -308,49 +293,49 @@ flexibleDataHelper <- function(prior, K, d, nMix, muMix, sigmaMix, lambdaMix, nO
 	}
 	if (nOther > 0) {														# second distribution
 		nOtherk <- as.vector(rmultinom(1, size = nOther, prob = prior))		# required number of observations in single classes
-print("nOtherk")
-print(nOtherk)
+# print("nOtherk")
+# print(nOtherk)
 		otherDataPool <- otherData <- list()
 		otherDataPool$x <- mixtureData(n = nOther*K, prior = 1, mu = list(muOther), sigma = sigmaOther*diag(d), lambda = lambdaOther)$x
 		otherDataPool$y <- mixtureLabels(otherDataPool$x, prior = prior, mu = muMix, sigma = lapply(sigmaMix, function(x) x*diag(d)), lambda = lambdaMix)
 		nk <- as.vector(table(otherDataPool$y))
-print("nk")
-print(nk)
+# print("nk")
+# print(nk)
 		bayesClassOther <- mixturePosterior(muOther, prior = prior, mu = muMix, sigma = lapply(sigmaMix, function(x) x*diag(d)), lambda = lambdaMix)
 		z <- 0
 		while (any(nk < nOtherk)) {		# if there are too few observations from at least one class draw more observations until there are enough
 			z <- z + 1
+			if (z > 25)
+				stop("adequate sampling from second distribution not feasible")			
 			maxk <- which(nk < nOtherk) # classes where observations are missing
-print(maxk)
+# print(maxk)
 			lambdaOther <- rowSums(bayesClassOther[, maxk, drop = FALSE])
-print(lambdaOther)
+# print(lambdaOther)
 			lambdaOther <- lambdaOther/sum(lambdaOther)
 			xnew <- mixtureData(n = nOther, prior = 1, mu = list(muOther), sigma = sigmaOther*diag(d), lambda = lambdaOther)$x
 			ynew <- mixtureLabels(xnew, prior = prior, mu = muMix, sigma = lapply(sigmaMix, function(x) x*diag(d)), lambda = lambdaMix)
 			otherDataPool$x <- rbind(otherDataPool$x, xnew)
 			otherDataPool$y <- factor(c(otherDataPool$y, ynew), levels = 1:K)
 			nk <- as.vector(table(otherDataPool$y))
-print("nk")
-print(nk)
-print("nOtherk - nk")
-print(nOtherk - nk)
-			if (z > 25)
-				stop("adequate sampling from second distribution not feasible")			
+# print("nk")
+# print(nk)
+# print("nOtherk - nk")
+# print(nOtherk - nk)
 		}
-print(z)
+# print(z)
 		indexk <- lapply(1:K, function(k) sample(nk[k], size = nOtherk[k]))
-print("indexk")		
-print(head(indexk))
+# print("indexk")		
+# print(head(indexk))
 		otherData$x <- do.call("rbind", lapply(1:K, function(k) otherDataPool$x[otherDataPool$y == k,, drop = FALSE][indexk[[k]],,drop=FALSE]))
-print("otherData$x")		
+# print("otherData$x")		
 		otherData$y <- factor(rep(1:K, nOtherk), levels = 1:K) 
-print("otherData$y")		
+# print("otherData$y")		
 	}
 	data <- list()
 	data$x <- rbind(mixData$x, otherData$x)
-print("data$x")		
+# print("data$x")		
 	data$y <- factor(c(mixData$y, otherData$y), levels = 1:K)
-print("data$y")		
+# print("data$y")		
 	return(data)
 }
 
